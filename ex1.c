154ba1da05f5e0f5dacaf8c985bec153c4bbfee6
stoshich <stoshich.stefan@yandex.ru>
2018-04-14 01:38:55 +0300

    ex1

--- ex1.c
+++ ex1.c
@@ -1,14 +1,10 @@
-#include <stdio.h>
-#include <string.h>
-void main()
-{
-    char first[100] = "слово ";
-    char *a = first;
-    char second[100] = "слово";
-    char *b = second;
-
-    strcat(a, b);
-    puts(a);
-    return 0;
-
+void push(struct node* head, int data) {
+	struct node* newNode =(struct node*) malloc(sizeof(struct node));
+	newNode->data = data;
+	newNode->next = NULL;
+	head = newNode;
+}
+void push_test() {
+	List head = build();
+	push(head, 1);
 }
